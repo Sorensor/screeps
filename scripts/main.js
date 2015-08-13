d837a9e645e9066b1558d2b3cadbfe103cee0d6c
Sorensor <CSorensen@hotmail.com.au>
2015-08-13 21:11:53 +1000

    Basic AI

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
-//this is a test
+//Base includes
+var factory = require('factory');
+var harvester = require('harvester');
+
+//Hack in harvesters
+var harvesters = [];
+for (var creep in Game.creeps)
+{
+    creep = Game.creeps[creep];
+    if (creep.memory.role == 'harvester')
+    {
+        harvesters.push(creep);
+    }
+}
+
+//Base AI
+factory.baseQueue();
+
+//Creep AI
+harvester
